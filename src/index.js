039908e6c9203f38c93cd9e95b77e32ae7398d09
Haseeb Majid <hello@haseebmajid.dev>
2020-05-08 11:24:17 +0100

    work

--- src/index.js
+++ src/index.js
@@ -10,25 +10,26 @@ async function main(args) {
   const subdomain = JSON.parse(subdomainData);
   const subdomains = [];
 
-  subdomain.forEach(alias => {
+  subdomain.forEach((alias) => {
     const normalizedAlias = alias
       .split(" ")
       .join("-")
       .toLowerCase();
     subdomains.push(`${normalizedAlias}.${args.mainDomain}`);
   });
+  console.log(subdomains);
 
   client
     .updateSite({
       site_id: args.siteId,
-      body: { domain_aliases: subdomains }
+      body: { domain_aliases: subdomains },
     })
-    .then(response => {
+    .then((response) => {
       console.log(
         `successfully updated domain aliases on Netlify, for ${response.name}.`
       );
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
       process.exit(1);
     });
@@ -40,31 +41,37 @@ const args = yargs
   .option("a", {
     alias: "accessToken",
     description: "The Netlify access token to use the Netlify API.",
-    demandOption: true
+    demandOption: true,
+  })
+  .option("c", {
+    alias: "createDomainEntries",
+    description: "If set to True, will also create domain entries in Netlify.",
+    boolean: true,
+    defult: false,
   })
   .option("f", {
     alias: "subdomainFile",
     description:
       "Path to JSON file, which contains the subdomain the netlify site should have.",
     demandOption: true,
-    default: "./subdomains.json"
+    default: "./subdomains.json",
   })
   .option("m", {
     alias: "mainDomain",
     description: "The main domain you want to create subdomain within.",
-    demandOption: true
+    demandOption: true,
   })
   .option("s", {
     alias: "siteId",
     description: "The site Id to add the subdomain to on Netlify.",
-    demandOption: true
+    demandOption: true,
   })
   .help("h")
   .alias("h", "help").argv;
 
 main(args)
   .then(() => {})
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
